ea33ec267a3987ee52c6a5ee76f6885a6aa35cbc
yxqsnz <yxqsnz@gmail.com>
2022-01-06 10:25:40 -0300

    Update Header.jsx

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -55,6 +55,7 @@ const NavTxt = styled.p`
 
 const Header = () => {
     const [scrollPosition, setScrollPosition] = useState(0);
+    console.log("changed scroll position:", scrollPosition);
     const [HeaderMode, setHeaderMode] = useState('header');
     const handleScroll = () => {
         const position = window.pageYOffset;
