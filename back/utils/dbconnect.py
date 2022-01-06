9153a13dc1dfa9701aacf8bae049dc5d83b24ce3
João Vitor de Aguiar Debossan <jvdebossan@gmail.com>
2022-01-06 16:43:22 -0300

    Delete back directory

--- back/utils/dbconnect.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv, find_dotenv  
-from os import getenv
-from pymongo import MongoClient
-
-def mongoConnect():
-    load_dotenv(find_dotenv())
-    uri = getenv('MONGO_URI')
-    
-    return MongoClient(uri)
